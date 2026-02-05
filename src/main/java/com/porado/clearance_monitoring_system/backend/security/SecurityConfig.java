5961a0676d0bf41c23a4f0dde3e20e797b19b8bf
Gabriel James Benedict <benok.loslos@gmail.com>
2026-02-05 12:10:50 +0800

    feat: add CorsConfig and SecurityConfig

---
+++ src/main/java/com/porado/clearance_monitoring_system/backend/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.porado.clearance_monitoring_system.backend.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsConfigurationSource corsConfigurationSource;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() //temporary
+                )
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
+    }
+
+}
